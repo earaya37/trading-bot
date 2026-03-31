68b5bee713934f948378deb233cdd771ce6ce8ff
Eric Araya Molina <47696534+earaya37@users.noreply.github.com>
2026-03-31 10:04:39 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,36 @@
+from binance.client import Client
+import os
 import time
 
-print("🚨🚨🚨 NUEVO CODIGO EJECUTANDO 🚨🚨🚨")
+API_KEY = os.getenv("API_KEY")
+API_SECRET = os.getenv("API_SECRET")
+
+client = Client(API_KEY, API_SECRET)
+
+symbol = "BCHUSDT"
+
+print("🚀 INICIO TEST ORDEN REAL")
 
 while True:
-    print("🔥 LOOP FUNCIONANDO")
+    try:
+        balance = client.futures_account_balance()
+        print("💰 Balance:", balance)
+
+        client.futures_change_leverage(symbol=symbol, leverage=5)
+
+        print("📤 Enviando orden...")
+
+        order = client.futures_create_order(
+            symbol=symbol,
+            side="BUY",
+            type="MARKET",
+            quantity=0.001
+        )
+
+        print("✅ ORDEN EJECUTADA:", order)
+        break
+
+    except Exception as e:
+        print("❌ ERROR REAL BINANCE:", e)
+
     time.sleep(5)
