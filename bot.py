ae852facc3440126ac8f00be716e70f93fe59cde
Eric Araya Molina <47696534+earaya37@users.noreply.github.com>
2026-03-31 09:46:52 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,5 @@
-from binance.client import Client
-import os
 import time
 
-API_KEY = os.getenv("API_KEY")
-API_SECRET = os.getenv("API_SECRET")
-
-client = Client(API_KEY, API_SECRET)
-
-symbol = "BTCUSDT"
-
 while True:
-    try:
-        print("Intentando trade...")
-
-        client.futures_change_leverage(symbol=symbol, leverage=5)
-
-        order = client.futures_create_order(
-            symbol=symbol,
-            side="BUY",
-            type="MARKET",
-            quantity=0.001
-        )
-
-        print("✅ TRADE EJECUTADO:", order)
-
-        break
-
-    except Exception as e:
-        print("❌ ERROR:", e)
-
+    print("🔥 BOT VIVO LOOP OK")
     time.sleep(5)
